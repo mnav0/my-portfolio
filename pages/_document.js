0096440a9d33ed84a739ba14c6a43795f8c323dd
Maggie Navracruz <mnavracruz@gmail.com>
2022-03-08 23:17:09 -0500

    added custom document page to generate styles server side

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, body, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    function handleCollectStyles(App) {
+      return props => {
+        return sheet.collectStyles(<App {...props} />);
+      };
+    }
+
+    const page = renderPage(App => handleCollectStyles(App));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
